1bdd830cfa22894f28a62efffa3381e5b4e2d73b
Daniel Vaz Volponi <daniel.volponi@nb-will0279.avista.local>
2022-08-03 13:34:58 -0300

    Set up uproject initial organization and metadata Add setup.py to turn installable Add __main__ for executable Add __init__ for package closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Daniel Volponi",
+    packages=find_packages(),
+)
